ef07ae054a59000e2275f3d40b4e08f49eae3c0a
yadeepak <deepaky19196@gmail.com>
2020-03-27 23:42:06 +0530

    error solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,7 +40,6 @@
                 @guest
                   <li><a href="{{ route('login') }}" title="Login">Login</a></li>
                   <li><a href="{{ route('register') }}" title="Register">Register</a></li>
-                  <li><a href="" title="Company Login">Company Login</a></li>
                 @else
                   <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">

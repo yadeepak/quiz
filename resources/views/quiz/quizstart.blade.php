7fdc82f756dd290d6cf12e2ae4e53293dff42023
yadeepak <deepaky19196@gmail.com>
2020-05-19 03:59:25 +0530

    Quiz design

--- resources/views/quiz/quizstart.blade.php
+++ resources/views/quiz/quizstart.blade.php
@@ -9,17 +9,40 @@
                         ]); ?>
 </script>
 <style>
+    body{
+        background-color: #ffffff;
+    }
+    .rowMain{
+        border: 1px solid #d4d4d4;
+    }
    .countstyle{
-    padding:10px;
-    border:1px solid;
-    text-decoration:none;
-    margin-left:5px;
-    line-height:3.6;
+    color: #fff;
+   }
+   .parentCount{
+    width: 29px;
+    color: #fff;
+    text-decoration: none;
+    display: inline-block;
+    padding: 3px;
+    line-height: 21px;
+    border-radius: 50%;
+    border: 1px solid;
+    text-align: center;
+    margin-bottom: 24px;
+    margin-left: 15px;
     cursor: pointer;
+   }
+   .parentBlock{
+    min-height: 317px;
+    max-height: 317px;
+    overflow: auto;
    }
    .active{
     background: #424242;
-    color: #fff;
+    color: #fff!important;
+   }
+   .active > a{
+    color: #fff!important;
    }
    .btn-outline-success{
     width: 200px;
@@ -32,14 +55,11 @@ color: #fff;
 background: #424242;
 }  
 
-.questions {
-
-background-color: #B2EBF2;
-
+#ques{
+    margin-left: 10em;
 }
 
 .quesno {
-
     color: #03A9F4; 
 }
 .mainque {
@@ -57,10 +77,9 @@ input[type="radio"] {
  display:none;
 }
 input[type="radio"]:checked+label {
-  border-bottom: 2px solid #222222;
-  color: red;
-  font-size: 18px;
-  padding-bottom: 0px;
+    color: #0dbd2c;
+    font-size: 18px;
+    padding-bottom: 0px;
   
 }
 
@@ -75,9 +94,30 @@ label {
 margin-top : 10px;
 
 } 
+.quesOverview{
+    color: #fff; 
+    padding-left:10px;
+}
+.dflex {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #e2e2e2;
+}
+.attemptedColor{
+    background: #009e00;
+}
+.attempted{
+    background: #009e00;
+    line-height: 0px;
+    margin-bottom: -9px;
+    padding: 13px;
 
-
-
+}
+.unattempted{
+    line-height: 0px;
+    margin-bottom: -9px;
+    padding: 13px;
+}
 
   </style>
 @endsection
@@ -117,59 +157,42 @@ margin-top : 10px;
 @endsection
 
 @section('content')
-<div class="container questions">
+<div class="container-fluid questions">
 <br>
-    <div class="row">
-        <div class="col-md-3" style="border-right:1px solid;">
-        <input type="hidden" class="custom-control-input" id="timer" name="timer" value="{{$topics['timer']}}">
-            <div class="">
-            
-                <?php for($i=0;$i<count($questions);$i++) { ?>
-                    <a class="countstyle active{{$i}}" id="<?php echo $i; ?>no" onClick="showDiv(<?php echo $i; ?>);" ><?php echo $i+1; ?></a>
-                <?php
-                    if ( (($i+1) % 5) == 0)
-                    {
-                        echo "<br>";
-                    }
-                
-                } ?>
-            
-            </div>
+    <div class="row rowMain">
+       
 
-        </div>
-
-
-        <?php // var_dump($questions); exit(); ?>
-
-        <div class="col-md-9">
+        <div class="col-md-9" >
         <div class="main" id="ques">
         
             <form action="{{route('submitTest')}}" method="post" id="form">
             {{csrf_field()}}      
             <?php for($i=0;$i<count($questions);$i++){ ?>
                 <div class="quesdiv globalques qustion{{$i}}" style="display: none">
-                <h4 class="quesno"><b>Question No . </b></h4><br>
-                <h1 class="mainque"><b>{{$i+1}} :- {{$questions[$i]->question}}</b></h1>
+               <div class="dflex"> <h4 class="quesno"><b>Question No . {{$i+1}}</b></h4>
+                <h4 class="quesno pull-right"><b>Single Choice Question</b></h4>
+               </div>
+                <h1 class="mainque"><b> {{$questions[$i]->question}}</b></h1>
                 @if($round==1)
 
-                <div class="custom-control custom-radio radio-holder">
+                <div class="custom-control custom-radio radio-holder optionsClick" data-id={{$i}}>
                 <input type="radio" class="custom-control-input" id="a" name="optradio{{$i}}" value="a">
                 
                 <label class="custom-control-label pad" for="a"><i class="fa fa-check"></i> {{$questions[$i]->a}}</label>
 
                 </div>
                     
-                <div class="custom-control custom-radio radio-holder">
+                <div class="custom-control custom-radio radio-holder optionsClick" data-id={{$i}}>
                 <input type="radio" class="custom-control-input" id="b" name="optradio{{$i}}" value="b">
                 <label class="custom-control-label pad" for="b"><i class="fa fa-check"></i> {{$questions[$i]->b}}</label>
                 </div>
                 
-                <div class="custom-control custom-radio radio-holder">
+                <div class="custom-control custom-radio radio-holder optionsClick" data-id={{$i}}>
                 <input type="radio" class="custom-control-input" id="c" name="optradio{{$i}}" value="c">
                 <label class="custom-control-label pad" for="c"><i class="fa fa-check"></i> {{$questions[$i]->c}}</label>
                 </div>
                 
-                <div class="custom-control custom-radio radio-holder">
+                <div class="custom-control custom-radio radio-holder optionsClick" data-id={{$i+1}}>
                 <input type="radio" class="custom-control-input" id="d" name="optradio{{$i}}" value="d">
                 <label class="custom-control-label pad" for="d"><i class="fa fa-check"></i> {{$questions[$i]->d}}</label>
                 </div>
@@ -179,12 +202,32 @@ margin-top : 10px;
                 </div>
             <?php } ?>
             <br>
-            <button type="submit" class="btn btn-primary btn-lg pull-center submitForm" style="width: 200px;">Submit</button>
+            <button type="submit" class="btn btn-primary  pull-center submitForm" style="width: 200px;">Submit</button>
             
             <br></br><br>
             </form>
            
         </div>
+        </div>
+        <div class="col-md-3" style="background: #009fe2;padding-bottom: 24px;">
+        <input type="hidden" class="custom-control-input" id="timer" name="timer" value="{{$topics['timer']}}">
+        <h4 class="quesOverview"><b>Question Overview </b></h4><br>
+         
+        <div class="parentBlock">
+            
+                <?php for($i=0;$i<30;$i++) { ?>
+                   <span class="parentCount active{{$i}} " onClick="showDiv(<?php echo $i; ?>);"> <a class="countstyle" id="<?php echo $i; ?>no"  ><?php echo $i+1; ?></a>
+                   </span> <?php
+                    if ( (($i+1) % 5) == 0)
+                    {
+                        echo "<br>";
+                    }
+                
+                } ?>
+            
+            </div>
+            <span class="parentCount attempted"></span> <span class="countstyle">Attempted</span>
+            <span class="parentCount unattempted"></span> <span class="countstyle">Unattempted</span>
         </div>
     </div>
 </div>
@@ -210,82 +253,94 @@ $(document).ready(function(){
     //console.log($check.prop("checked"));
   });
 
+  $('.optionsClick').click(function(){
+    const id = $(this).data('id');
+    $('.active'+id).addClass('attemptedColor');
+});
 
+$('.submitForm').click(function(e){
+    e.preventDefault();
+    const check = confirm("Are you sure you want to submit the exam , click ok to confirm");
+if(check){
+    $('#form').submit();
+}
+})
 
 
 
 })
 function showDiv(id){
-    $('.countstyle').removeClass('active');
+    $('.parentCount').removeClass('active');
     $('.globalques').hide();
     $('.active'+id).addClass('active');
     $('.qustion'+id).fadeIn();
 
 }
 
+
  
 
-// document.onkeydown = function() {            
-//     switch (event.keyCode) { 
-//         case 116 : //F5 button
-//             event.returnValue = false;
-//             event.keyCode = 0;
-//             alert('If you refresh the page your data will be submitted.');
-//             return false; 
-//         case 82 : //R button
-//             if (event.ctrlKey) { 
-//                 event.returnValue = false; 
-//                 event.keyCode = 0;  
-//             alert('If you refresh the page your data will be submitted.');
-//                 return false; 
-//             } 
-//             case 123 : //f12 button
-//                 event.returnValue = false; 
-//                 event.keyCode = 0;  
-//                 return false; 
+document.onkeydown = function() {            
+    switch (event.keyCode) { 
+        case 116 : //F5 button
+            event.returnValue = false;
+            event.keyCode = 0;
+            alert('If you refresh the page your data will be submitted.');
+            return false; 
+        case 82 : //R button
+            if (event.ctrlKey) { 
+                event.returnValue = false; 
+                event.keyCode = 0;  
+            alert('If you refresh the page your data will be submitted.');
+                return false; 
+            } 
+            case 123 : //f12 button
+                event.returnValue = false; 
+                event.keyCode = 0;  
+                return false; 
             
-//     }
-// }
+    }
+}
 
 
   
-// window.onload = function() {
-//     document.addEventListener("contextmenu", function(e){
-//       e.preventDefault();
-//     }, false);
-//     document.addEventListener("keydown", function(e) {
-//     //document.onkeydown = function(e) {
-//       // "I" key
-//       if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-//         disabledEvent(e);
-//       }
-//       // "J" key
-//       if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-//         disabledEvent(e);
-//       }
-//       // "S" key + macOS
-//       if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-//         disabledEvent(e);
-//       }
-//       // "U" key
-//       if (e.ctrlKey && e.keyCode == 85) {
-//         disabledEvent(e);
-//       }
-//       // "F12" key
-//       if (event.keyCode == 123) {
-//         disabledEvent(e);
-//       }
-//     }, false);
-//     function disabledEvent(e){
-//       if (e.stopPropagation){
-//         e.stopPropagation();
-//       } else if (window.event){
-//         window.event.cancelBubble = true;
-//       }
-//       e.preventDefault();
-//       return false;
-//     }
-//   };
+window.onload = function() {
+    document.addEventListener("contextmenu", function(e){
+      e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+      // "I" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        disabledEvent(e);
+      }
+      // "J" key
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        disabledEvent(e);
+      }
+      // "S" key + macOS
+      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+      }
+      // "U" key
+      if (e.ctrlKey && e.keyCode == 85) {
+        disabledEvent(e);
+      }
+      // "F12" key
+      if (event.keyCode == 123) {
+        disabledEvent(e);
+      }
+    }, false);
+    function disabledEvent(e){
+      if (e.stopPropagation){
+        e.stopPropagation();
+      } else if (window.event){
+        window.event.cancelBubble = true;
+      }
+      e.preventDefault();
+      return false;
+    }
+  };
 
 var counter = {};
 window.addEventListener("load", function(){
